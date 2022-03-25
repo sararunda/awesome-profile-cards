3bea9382cb8ab30d3dafe01f4051b3e2a7d1780f
Ce <celiarosalescaballero@gmail.com>
2022-03-25 12:33:30 +0100

    add js

--- src/js/_rellena.js
+++ src/js/_rellena.js
@@ -1,23 +1,27 @@
-'use strict'
+'use strict';
 
-console.log("miiiiiiiiiiii");
 
 
 const fill = document.querySelector('.js-desplegable');
 const form2 = document.querySelector('.js-fieldset');
-const arrowUp = document.querySelector('.fa-angle-up') ;
-const arrowDown = document.querySelector('.fa-angle-down');
+const arrowUp = document.querySelector('.js-icon-rellena') ;
 
-function hiddeFieldset() { 
-  if (form2.classList.contains('collapsed')) {
-    form2.classList.remove('collapsed')
+
+function hiddeFieldset(event) { 
+  event.preventDefault();
+  if (form2.classList.contains('collapsed') && arrowUp.classList.contains('fa-transform')) {
+
+    form2.classList.remove('collapsed');
+    arrowUp.classList.remove('fa-transform')
     
     
   } else {
-    form2.classList.add('collapsed')
+    form2.classList.add('collapsed');
+    arrowUp.classList.add('fa-transform');
+    
   } }
 
-  fill.addEventListener('click', hiddeFieldset) 
+  fill.addEventListener('click', hiddeFieldset) ;
 
 
 
