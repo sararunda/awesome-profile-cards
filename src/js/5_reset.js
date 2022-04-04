7a427589089235e9b61fdad0eced4aacdf9bc7b5
miaor5 <ortiz.maria.mero@gmail.com>
2022-04-04 14:00:44 +0200

    try reset

---
+++ src/js/5_reset.js
@@ -0,0 +1,11 @@
+'use strict';
+const buttonReset= document.querySelector ('.js_buttonReset');
+let resetInput=document.querySelectorAll('.js_resetInput');
+
+
+function handleClickReset(event){
+ event.preventDefault();
+ resetInput.value= '';
+}
+
+buttonReset.addEventListener('click', handleClickReset);
