0c0efc55232c5a44bce91bf8895a49e19089405c
Ce <celiarosalescaballero@gmail.com>
2022-04-05 14:02:37 +0200

    add reset

--- src/js/5_reset.js
+++ src/js/5_reset.js
@@ -3,9 +3,26 @@ const buttonReset= document.querySelector ('.js_buttonReset');
 let resetInput=document.querySelectorAll('.js_resetInput');
 
 
+function resetCard() {
+ previewName.innerHTML = 'Nombre Apellido';
+ previewJob.innerHTML = 'Front-end developer';
+};
+
+
+
+
 function handleClickReset(event){
  event.preventDefault();
- resetInput.value= '';
-}
+ console.log('holii');
+ for (const item of resetInput) {
+     item.value = '';
+ }
+
+resetCard();
+
+};
+
+
+
 
 buttonReset.addEventListener('click', handleClickReset);
