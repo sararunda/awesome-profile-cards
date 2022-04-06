a1f5d725c753d2eda769516e35964d79d4d5ecb2
tanferest <tanferest@gmail.com>
2022-04-06 19:01:26 +0200

    fix display on collapsibles and css errors

--- src/js/1_collapsible.js
+++ src/js/1_collapsible.js
@@ -8,22 +8,56 @@ function hiddeFieldset(event) {
   ) {
     form2.classList.remove('collapsed');
     arrowUp.classList.remove('fa-transform');
+    design.classList.add('collapsed');
+    designArrow.classList.add('fa-transform');
+    share.classList.add('collapsed');
+    shareArrow.classList.add('fa-transform')
+
   } else {
     form2.classList.add('collapsed');
     arrowUp.classList.add('fa-transform');
   }
 }
 
-designLegend.addEventListener('click', (event) => {
+function handleClickDesign (event) {
   event.preventDefault();
-  design.classList.toggle('collapsed');
-  designArrow.classList.toggle('arrowDown');
-});
+  if (
+    design.classList.contains('collapsed') &&
+    designArrow.classList.contains('fa-transform')
+  ) {
+    design.classList.remove('collapsed');
+    designArrow.classList.remove('fa-transform');
+    form2.classList.add('collapsed');
+    arrowUp.classList.add('fa-transform');
+    share.classList.add('collapsed');
+    shareArrow.classList.add('fa-transform');
+
+  } else {
+    design.classList.add('collapsed');
+    designArrow.classList.add('fa-transform');
+  }
+}
+
+function handleClickShare (event) {
+  event.preventDefault();
+  if (
+    share.classList.contains('collapsed') &&
+    shareArrow.classList.contains('fa-transform')
+  ) {
+    share.classList.remove('collapsed');
+    shareArrow.classList.remove('fa-transform');
+    design.classList.add('collapsed');
+    designArrow.classList.add('fa-transform');
+    form2.classList.add('collapsed');
+    arrowUp.classList.add('fa-transform');
+
+  } else {
+    share.classList.add('collapsed');
+    shareArrow.classList.add('fa-transform');
+  }
+}
+
 
 fill.addEventListener('click', hiddeFieldset);
-
-shareLegend.addEventListener('click', (event) => {
-  event.preventDefault();
-  share.classList.toggle('collapsed');
-  shareArrow.classList.toggle('arrowDown');
-});
+designLegend.addEventListener('click', handleClickDesign);
+shareLegend.addEventListener('click', handleClickShare);
