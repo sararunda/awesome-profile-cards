08fc1a54a86d2f3ce32c2864717e6f4e68d51dc3
tanferest <tanferest@gmail.com>
2022-04-12 12:26:55 +0200

    add error message

--- src/js/6_card.js
+++ src/js/6_card.js
@@ -7,25 +7,35 @@ function changeColorButton(event) {
 
 function handlebntCreate(event) {
   event.preventDefault();
-  if (data.name === '' || data.job === '' || data.email === '' || data.phone === '' || data.linkedin === '' || data.github === '' || data.photo === '') {
+  if (
+    data.name === '' ||
+    data.job === '' ||
+    data.email === '' ||
+    data.phone === '' ||
+    data.linkedin === '' ||
+    data.github === '' ||
+    data.photo === ''
+  ) {
     messageCard.innerHTML = 'Debe rellenar todos los campos';
+  } else {
+    fetch('https://awesome-profile-cards.herokuapp.com/card/', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'content-type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((cardData) => {
+        console.log(cardData);
+        const url = cardData.cardURL;
+        generatedUrl.innerHTML = url;
+        generatedUrl.href = url;
+        shareOnTwitter.href = `https://twitter.com/intent/tweet?text=${url}`;
+      });
+    messageCard.innerHTML = '';
+    changeColorButton();
   }
-  fetch('https://awesome-profile-cards.herokuapp.com/card/', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'content-type': 'application/json',
-    },
-  })
-    .then((response) => response.json())
-    .then((cardData) => {
-      console.log(cardData);
-      const url = cardData.cardURL;
-      generatedUrl.innerHTML = url;
-      generatedUrl.href = url;
-      shareOnTwitter.href = `https://twitter.com/intent/tweet?text=${url}`;
-    });
-  changeColorButton();
 }
 
 btn.addEventListener('click', handlebntCreate);
